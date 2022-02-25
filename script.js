0219a8c0ff21b0d5ed0d9cac1e036bab35ddc441
esuter19 <59849377+esuter19@users.noreply.github.com>
2022-02-24 21:39:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
-require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
-        var map = new Map({
-          basemap: "topo-vector"
-        });
+require([
+      "esri/Map",
+      "esri/layers/FeatureLayer",
+      "esri/views/MapView",
+      "dojo/domReady!"
+    ], function(
+      Map,
+      FeatureLayer,
+      MapView
+    ) {
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 4,
-          center: [15, 65] // longitude, latitude
-        });
+      // Create the map
+      var map = new Map({
+        basemap: "gray"
       });
+
+      // Create the MapView
+      var view = new MapView({
+        container: "viewDiv",
+        map: map,
+        center:[-90.422, 38.644],
+        zoom: 10
+      });
+      var template = {
+        title: "School District: {SCHOOL_DISTRICT}",
+        content: [{
+          type: "fields",
+          fieldInfos: [{
+            fieldName: "OBJECTID",
+            label: "ID: ",
+            visible: true
+          }, {
+            fieldName: "SCHOOLCODE",
+            label: "Code: ",
+            visible: true
+          }
+                      ]
+        }]
+      };
+      var featureLayer = new FeatureLayer({
+        url: "http://maps.stlouisco.com/arcgis/rest/services/OpenData/OpenData/FeatureServer/4",
+        outFields: ["*"],
+        popupTemplate: template
+      });
+  
+      map.add(featureLayer);
+    });
